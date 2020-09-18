b9d56e2095a197de5b45a22c6124ee13b366616d
Yolo <vincentrudel@hotmail.com>
2020-09-18 17:40:55 -0400

    implemented multiple images

--- make_emission.py
+++ make_emission.py
@@ -1,48 +1,54 @@
 from PIL import Image
 import numpy as np
-#from tkinter.filedialog import askopenfilename
-#filename = askopenfilename(filetypes=[("PNG","*.png")])
 
+from tkinter.filedialog import askopenfilename
+filename = askopenfilename(filetypes=[("PNG","*.png")])
+from copy import deepcopy
 import re
 
-#arr = np.array(Image.open(filename))
+arr = np.array(Image.open(filename))
 
 print("Please enter RGB values of color to change")
 
 red =-1
 green = -1
 blue = -1
-p = re.compile("([0-1]+\d+\d|2[0-4][0-9]|2[5][0-5])[\s,;:]{2}\0")
+
 colors = []
 inp = None
 while inp !="c":
-	inp = input("Add another color (c to continue):\n").lower()
+	inp = input("Add another color (c to continue):\n").rstrip()
 	invalid = True
-	match = re.match(p, inp)
+	if inp == "c":
+		break
+	try:
+		color = [int(x) for x in inp.split(" ")]
+		for x in color:
+			if x > 255 or x < 0:
+				raise "Invalid color"
+		colors.append(color)
+	except Exception as e:
+		print(e)
+		print("Invalid color")
+
 	
-	print(match)
-exit()
-while not (red > -1 and red < 256):
-	red = int(input("R: ").rstrip())
-while not (green > -1 and red < 256): 
-	green = int(input("G: ").rstrip())
-	
-while not (blue > -1 and blue < 256):
-	blue = int(input("B: ").rstrip())
 
 height = len(arr)
 width = len(arr[0])
+i=1
+for col in colors:
+	arr1 = deepcopy(arr)
+	for r in range(height):
+		for c in range(width):
+			if np.array_equal(arr1[r][c][0:3], [col[0],col[1],col[2]]):
+				arr1[r][c] = [255,255,255,255]
+			else:
+				arr1[r][c] = [0,0,0,255]
 
-for r in range(height):
-	for c in range(width):
-		if np.array_equal(arr[r][c][0:3], [red,green,blue]):
-			arr[r][c] = [255,255,255,255]
-		else:
-			arr[r][c] = [0,0,0,255]
-filename = filename.replace(".png", "_Emission.png")			
-Image.fromarray(arr).save( filename, "PNG")
-print(filename + " saved!")
-
+	filename1 = filename.replace(".png", "_Emission"+str(i)+".png")			
+	Image.fromarray(arr1).save( filename1, "PNG")
+	print(filename1 + " saved!")
+	i+=1
 
 
 
